1b6301ea4d5c4ce02d35d7aa7997edd266483d8f
SanQuan Prioleau <sanquan.prioleau@dartmouth.edu>
2021-01-12 17:01:46 -0500

    Use div and lists for dropdowns

--- src/app/components/DropdownList.js
+++ src/app/components/DropdownList.js
@@ -1,43 +1,26 @@
 import React from "react";
 import { composeClasses } from "../utilities/utilityFunctions";
-import { updateLanguage, updateTheme } from "../state-provider/actions/actionCreators";
-import { selectLanguage, selectTheme } from "../state-provider/selectors/selectors";
-import { useDispatch, useSelector } from "react-redux";
 
-const DropdownList = ({ listItems, dropdownKey, addClass }) => {
-	const language = useSelector(selectLanguage);
-	const theme = useSelector(selectTheme);
-	const dispatch = useDispatch();
-
+const DropdownList = ({ listItems, value, open, addClass, labelOnClick, listItemOnClick }) => {
 	const dropdownListClasses = {
 		"dropdown-list": "",
 		[addClass]: addClass ? true : null,
 	};
 
-	const componentProperties = {
-		language: {
-			function: (language) => updateLanguage(language),
-			value: language,
-		},
-		theme: {
-			function: (theme) => updateTheme(theme),
-			value: theme,
-		},
-	};
-
-	const onChange = (e) => {
-		dispatch(componentProperties[dropdownKey].function(e.target.value));
-	};
-
 	return (
 		<div className={composeClasses(dropdownListClasses)}>
-			<select className="dropdown-list__list" value={componentProperties[dropdownKey].value} onChange={onChange}>
-				{listItems.map(({ name, label }) => (
-					<option key={name} value={name} className="list-item">
-						{label}
-					</option>
-				))}
-			</select>
+			<div className="dropdown-list__label" onClick={labelOnClick}>
+				{value}
+			</div>
+			{open && (
+				<ul className="dropdown-list__list">
+					{listItems.map(({ name, label }) => (
+						<li key={name} className="list-item" onClick={() => listItemOnClick(name)}>
+							{label}
+						</li>
+					))}
+				</ul>
+			)}
 		</div>
 	);
 };
