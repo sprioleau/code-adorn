9e804a53da6ab55d2a0b49bfd336e5ed18509a29
SanQuan Prioleau <sanquan.prioleau@dartmouth.edu>
2021-01-14 12:15:34 -0500

    Return to homepage if no screenshot is present

--- src/app/components/Screenshot.js
+++ src/app/components/Screenshot.js
@@ -1,8 +1,10 @@
 import React from "react";
+import { Redirect } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 const Screenshot = () => {
 	const url = useSelector((state) => state.screenshotUrl);
+	if (!url) return <Redirect to="/" />;
 	return <img src={url} alt="code-adorn-screenshot" width="100%" />;
 };
 
